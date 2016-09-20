0a6b8565a1bf8045b95e495f6d4cb674a1ec7c15
Jonathan Whitmore <jonathan.b.whitmore@gmail.com>
2016-09-20 16:11:29 -0700

    Version bump

--- automation.rb
+++ automation.rb
@@ -1,9 +1,9 @@
 class Automation < Formula
   desc "A series of scripts that make your workflow faster and better!"
   homepage "https://github.com/jbwhit/automation"
-  url "https://github.com/jbwhit/automation/archive/0.0.2.tar.gz"
-  version "0.0.2"
-  sha256 "69b0783b1be3d52034cc719843efce1a798fa4996760cc71c8ee99b407bfff7f"
+  url "https://github.com/jbwhit/automation/archive/0.0.3.zip"
+  version "0.0.3"
+  sha256 "808e1d449d68aedc1f6d8cc83187975f64786addaa0b994c5a507f6f70f0a2e9"
 
   def install
     bin.install "svds-create-project"
